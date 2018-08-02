e078196ab3e231e11d1bd8f4cd31a7687d12f60c
Malik Olivier Boussejra <malik@boussejra.com>
2018-08-03 08:56:29 +0900

    Simplify even more

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
 extern crate reqwest;
 extern crate select;
 
-use select::{
-    document::Document, predicate::{Class, Name},
-};
+use select::{document::Document, predicate::Class};
 
 fn main() {
     let document = Document::from("");
     for node in document.find(Class("foo")) {
         let mut img_url = None;
-        for img_node in node.find(Name("bar")) {
-            if let Some(img_link) = img_node.attr("src") {
-                img_url = img_link.as_ref();
-                break;
-            }
+        if let Some(img_link) = node.attr("src") {
+            img_url = img_link.as_ref();
         }
     }
 }
